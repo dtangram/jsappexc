4932343e007a50893ba42799aa7ab6c5d23cf7fb
Douglas T. Angram <douglast.angram@dothans-mbp.attlocal.net>
2021-03-06 15:09:39 -0600

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import backgrounds from './backgrounds.js';
+import Select from 'react-select';
+import './App.css';
+
+const App = () => {
+  const back = backgrounds.map(item => item);
+  console.log(back);
+  console.log(back[0].value);
+
+  const [setBCKValue, bckValue] = useState(back.value);
+  const [setBCKType, bckType] = useState(back.type);
+
+  const handleSelectChange = (event) => {
+    bckValue(event.value);
+    bckType(event.type);
+  };
+
+  return (
+    <div className="App-header">
+      <img src={logo} alt="logo" />
+      <video controls autoPlay muted>
+        <source
+          src={`../${setBCKValue}.${setBCKType}`}
+          type={`video/${setBCKType}`}
+        />
+          Your browser does not support the video tag.
+      </video>
+
+      <img src={`../${setBCKValue}.${setBCKType}`} alt={back.value} />
+      <Select
+        name="select-bg"
+        id="select-bg"
+        placeholder="--Please choose an option--"
+        options={back}
+        onChange={handleSelectChange}
+      />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/backgrounds.js
@@ -0,0 +1,29 @@
+const backgrounds = [
+  {
+    value: "bunny-ears",
+    label: "Bunny Ears",
+    type: "mp4"
+  },
+  {
+    value: "outer-space",
+    label: "Outer Space",
+    type: "mp4"
+  },
+  {
+    value: "superhero-yeti",
+    label: "Superhero Yeti",
+    type: "mp4"
+  },
+  {
+    value: "toboggan",
+    label: "Toboggan",
+    type: "jpg"
+  },
+  {
+    value: "yeti-village",
+    label: "Yeti Village",
+    type: "jpg"
+  },
+];
+
+export default backgrounds;
